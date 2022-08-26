5b4e0785b3a52f2a4acaddbc473872a69525d233
hadyhefny <hady.hefny@gmail.com>
2022-08-26 13:23:48 +0200

    - add project dependencies - init hilt

---
+++ app/src/main/java/com/hefny/hady/pixabaygallery/core/application/PixabayApplication.kt
@@ -0,0 +1,8 @@
+package com.hefny.hady.pixabaygallery.core.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PixabayApplication: Application() {
+}
