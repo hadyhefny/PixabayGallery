be5f47014f929d5bc7975d7d24e516468dbbf8d6
hadyhefny <hady.hefny@gmail.com>
2022-08-29 18:08:43 +0200

    bug fix: make viewmodel scope to fragment not shared

--- app/src/main/java/com/hefny/hady/pixabaygallery/modules/images/presentation/view/ImagesFragment.kt
+++ app/src/main/java/com/hefny/hady/pixabaygallery/modules/images/presentation/view/ImagesFragment.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
@@ -31,7 +31,7 @@ class ImagesFragment : Fragment() {
 
     @Inject
     lateinit var imagesLoadStateAdapter: ImagesLoadStateAdapter
-    private val imagesViewModel: ImagesViewModel by activityViewModels()
+    private val imagesViewModel: ImagesViewModel by viewModels()
     private val itemDecorator by lazy { SimpleDividerItemDecoration(requireContext()) }
     private lateinit var searchView: SearchView
     override fun onCreateView(
